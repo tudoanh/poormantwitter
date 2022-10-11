04d8731e2624efe57b2a51484dd09551514269fb
tudoanh <tu0703@gmail.com>
2022-10-11 13:43:22 +0700

    add some tests

--- twitter/tests.py
+++ twitter/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+from twitter.models import Tweet
+from rest_framework.test import APIClient
+
+client = APIClient()
+
+
+class TweetTestCase(TestCase):
+    """Test tweet CRUD with API endpoints"""
+
+    def test_tweet_create(self):
+        """Test tweet creation"""
+        client.post("/tweets/", {"author": "god", "content": "test"})
+        self.assertEqual(Tweet.objects.count(), 1)
+        self.assertEqual(Tweet.objects.first().author, "god")
+        self.assertEqual(Tweet.objects.first().content, "test")
+
+    def test_tweet_list(self):
+        """Test tweet list"""
+        client.post("/tweets/", {"author": "god", "content": "protect"})
+        client.post("/tweets/", {"author": "devil", "content": "chaos"})
+        client.post("/tweets/", {"author": "human", "content": "pure"})
+        response = client.get("/tweets/")
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(response.data[0]["author"], "human")
+        self.assertEqual(response.data[0]["content"], "pure")
+
+    def test_tweet_retrieve(self):
+        """Test tweet retrieve"""
+        client.post("/tweets/", {"author": "god", "content": "protect"})
+        client.post("/tweets/", {"author": "devil", "content": "chaos"})
+        client.post("/tweets/", {"author": "human", "content": "pure"})
+        response = client.get("/tweets/1/")
+        self.assertEqual(response.data["author"], "god")
+        self.assertEqual(response.data["content"], "protect")
