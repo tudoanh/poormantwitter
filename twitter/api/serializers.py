12cd7ddef0c61ddf83778800b3a93759f0dffdcc
tudoanh <tu0703@gmail.com>
2022-10-11 13:32:04 +0700

    add date

--- twitter/api/serializers.py
+++ twitter/api/serializers.py
@@ -6,3 +6,8 @@ class TweetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tweet
         fields = ("id", "author", "content", "created")
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['created'] = instance.created.strftime("%Y-%m-%d %H:%M:%S")
+        return rep
